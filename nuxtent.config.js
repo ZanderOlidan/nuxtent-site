83188d22c9b0d32592f82e84d8aef5326fd237ae
Zander Olidan <zander@playstudios.asia>
2018-08-27 16:09:21 +0800

    Testing multi

--- nuxtent.config.js
+++ nuxtent.config.js
@@ -1,12 +1,21 @@
 module.exports = {
-  content: {
-    permalink: '/songs/:slug',
-    page: '/_songs',
-    generate: [ // for static build
-      'get', 'getAll'
-    ],
-    isPost: false
-  },
+  // content: {
+  //   permalink: '/songs/:slug',
+  //   page: '/_songs',
+  //   generate: [ // for static build
+  //     'get', 'getAll'
+  //   ],
+  //   isPost: false
+  // },
+  content: [
+    "songs", {
+      permalink: '/songs/:slug',
+      page: '/_songs',
+      generate: [
+        'get', 'getAll'
+      ]
+    }
+  ],
   api: {
     baseURL: 'http://localhost:3000',
     browserBaseURL: 'https://jolly-lewin-dd1240.netlify.com'
